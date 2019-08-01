29469544f4ccaaf3509cedcfac54e28b6f399123
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-08-01 20:26:42 +0300

    done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card({ datum: { img, title }, children }) {
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      <img src="" className="card-img-top" alt="" />
+    <div className="card m-4" style={{ width: '18rem' }}>
+      <img
+        style={{ visibility: img ? 'visible' : 'hidden' }}
+        src={img}
+        className="card-img-top"
+        alt={title}
+      />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{children}</p>
         <a href="https://getbootstrap.com" className="btn btn-primary">
-          Go somewhere
+          Learn more about {title}
         </a>
       </div>
     </div>
